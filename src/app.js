8614fe49691b10f79871e8ae440941c53e0dd890
Salvador Hernandez <zhavaman@gmail.com>
2017-05-13 17:11:13 -0500

    #1 add http request code

--- src/app.js
+++ src/app.js
@@ -1,5 +1,10 @@
 export class App {
   constructor() {
     this.message = 'Aurelia JSON';
+    let client = new HttpClient();
+    client.get('package.json')
+      .then(data => {
+        console.log(data.description)
+      });
   }
 }
