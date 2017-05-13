34010be8ca60eda00ad369f454a2fda8b95c596f
enrique199413@gmail.com <enrique199413@gmail.com>
2017-05-13 18:53:32 -0500

    #1 add the function to add the building class

--- src/app.js
+++ src/app.js
@@ -1,12 +1,23 @@
+import {inject} from 'aurelia-framework'
 import {HttpClient} from 'aurelia-http-client';
+import {Building} from 'cierraloBuilding';
 
+@inject(Building)
 export class App {
   constructor() {
+    this.buildings = [];
+    this.title = 'Propiedades';
     this.message = 'Aurelia JSON';
     let client = new HttpClient();
     client.get('https://demo6292426.mockable.io/c-properties')
       .then(data => {
-        console.log(JSON.parse(data.response));
+        JSON.parse(data.response)[Object.keys(JSON.parse(data.response))[0]].forEach((building) => {
+          this.addBuilding(building);
+        });
       });
+  }
+  addBuilding(building) {
+    this.buildings.push(new Building(building.precio));
+    console.log(this.buildings);
   }
 }
