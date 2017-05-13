1c9ff760a8e2e048a87a97ffd5df05e1941d85f2
enrique199413@gmail.com <enrique199413@gmail.com>
2017-05-13 17:25:39 -0500

    #1 add the example for get JSON

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
+import {HttpClient} from 'aurelia-http-client';
+
 export class App {
   constructor() {
     this.message = 'Aurelia JSON';
     let client = new HttpClient();
-    client.get('package.json')
+    client.get('https://demo6292426.mockable.io/c-properties')
       .then(data => {
-        console.log(data.description)
+        console.log(JSON.parse(data.response));
       });
   }
 }
