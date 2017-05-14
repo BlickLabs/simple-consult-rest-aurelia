6241a1aaaad6a90ce5537e919f496416604092b2
enrique199413@gmail.com <enrique199413@gmail.com>
2017-05-13 19:09:38 -0500

    #1 add the bindable property in model

--- src/cierraloBuilding.js
+++ src/cierraloBuilding.js
@@ -5,6 +5,8 @@ import {customElement, bindable} from 'aurelia-framework';
 
 @customElement('cierralo-building')
 export class Building {
+  @bindable building;
+
   constructor (precio) {
     this.precio = precio;
   }
